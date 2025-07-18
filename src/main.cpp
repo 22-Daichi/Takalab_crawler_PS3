d1e8ac69fd38f46391d55bf9b2a17356f3c1d019
22-Daichi <tokyotechdai20@gmail.com>
2025-07-18 12:59:22 +0900

    割り込みを廃止

--- src/main.cpp
+++ src/main.cpp
@@ -14,33 +14,33 @@ const int leftWheelDirPin = 4; */
 
 const int rightWheelPwrPin = 5;
 const int leftWheelPwrPin = 16; // 強すぎ
-const int rightWheelDirPin = 17;
-const int leftWheelDirPin = 4;
+const int rightWheelDirPin = 4;
+const int leftWheelDirPin = 17;
 
-const int inputPin = 15; // 入力ピン（pullvdown）
-const int outputPin = 2; // 出力ピン
+const int inputPin = 25;  // 入力ピン（pullvdown）
+const int outputPin = 12; // 出力ピン
 
 int rightWheelPwr = 0;
 int leftWheelPwr = 0;
 bool rightWheelDir = 0;
 bool leftWheelDir = 0;
 
-int maxPwr = 50;
+int maxPwr = 250;
 
 int t = 0;
 
 volatile bool triggered = true;
 
-void IRAM_ATTR handleInterrupt()
+/* void IRAM_ATTR handleInterrupt()
 {
   currentTime = millis();
-  if (currentTime - lastInterruptTime > 50)
+  if (currentTime - lastInterruptTime > 500) // && analogRead(inputPin) < 100
   { // 50ms以上の間隔のみ有効
     triggered = true;
     digitalWrite(outputPin, LOW);
     lastInterruptTime = currentTime;
   }
-}
+} */
 
 void pinModeSetup()
 {
@@ -48,12 +48,12 @@ void pinModeSetup()
   pinMode(rightWheelDirPin, OUTPUT);
   pinMode(leftWheelPwrPin, OUTPUT);
   pinMode(leftWheelDirPin, OUTPUT);
+  pinMode(2, OUTPUT);
   digitalWrite(rightWheelPwrPin, LOW);
   digitalWrite(leftWheelPwrPin, LOW);
-  pinMode(inputPin, INPUT_PULLDOWN); // プルアップ入力
-  pinMode(outputPin, OUTPUT);        // 出力モード
-  digitalWrite(outputPin, LOW);      // 初期はLOW
-
+  pinMode(inputPin, INPUT);     // 外付け抵抗によってプルダウン
+  pinMode(outputPin, OUTPUT);   // 出力モード
+  digitalWrite(outputPin, LOW); // 初期はLOW
 }
 
 void pwmSetup()
@@ -78,7 +78,7 @@ void setup()
   Ps3.begin("5c:6d:20:2b:b2:f9"); // 9c:9c:1f:d0:04:be
   Serial.println("Ready.");
   pwmSetup();
-  attachInterrupt(digitalPinToInterrupt(inputPin), handleInterrupt, FALLING);
+  // attachInterrupt(digitalPinToInterrupt(inputPin), handleInterrupt, FALLING);
 }
 
 void WheelPwrOn()
@@ -107,7 +107,7 @@ void WheelPwrOn()
     ledcWrite(leftPwmCh, leftWheelPwr * (-1));
     digitalWrite(leftWheelDirPin, leftWheelDir);
   }
-  Serial.println(rightWheelPwr);
+  // Serial.println(rightWheelPwr);
 }
 
 void WheelPwrOff()
@@ -170,10 +170,10 @@ void setWheelPwr()
 
 void loop()
 {
-  if (triggered && digitalRead(inputPin) == HIGH) // スイッチ押されてない
+  if (analogRead(inputPin) < 100)
   {
-    triggered = false;
-    digitalWrite(outputPin, HIGH);
+    triggered = true;
+    digitalWrite(outputPin, LOW);
   }
   if (Ps3.isConnected())
   {
@@ -183,8 +183,16 @@ void loop()
   }
   if (!Ps3.isConnected())
   {
-    Serial.println("NoConnection");
-    // wheelPwrOff();
+    // Serial.println("NoConnection");
+    //  wheelPwrOff();
   }
+  if (triggered == 1 && analogRead(inputPin) == 4095) // スイッチ押されてない
+  {
+    triggered = false;
+    digitalWrite(outputPin, HIGH);
+  }
+  digitalWrite(2, HIGH);
+  Serial.print(triggered);
+  Serial.println(analogRead(inputPin));
   delay(50);
 }
